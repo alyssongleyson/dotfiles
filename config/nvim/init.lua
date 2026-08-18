3e08a3b2ce17ca8fece20b90aba330e5178b234a
Álysson Gleyson da Silva <alyssongleyson.dev@gmail.com>
2026-08-18 01:06:13 -0300

    feat(nvim): bootstrap lazy.nvim plugin manager

---
+++ config/nvim/init.lua
@@ -0,0 +1,17 @@
+-- Ensure lazy.nvim package manager is installed
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Initialize lazy.nvim with an empty plugin list
+require("lazy").setup({})
+
