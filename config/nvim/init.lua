557a832ca7fd5e13a9c6094855fdc34fb9879065
Álysson Gleyson da Silva <alyssongleyson.dev@gmail.com>
2026-08-19 21:48:16 -0300

    feat(nvim): configure core editor options

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -15,3 +15,7 @@ vim.opt.rtp:prepend(lazypath)
 -- Initialize lazy.nvim with an empty plugin list
 require("lazy").setup({})
 
+-- General editor options
+vim.opt.number = true         -- Displays line numbers on the left gutter
+vim.opt.termguicolors = true  -- Enables 24-bit RGB color support in the TUI
+vim.opt.conceallevel = 2      -- Hides markup elements (e.g., Markdown) for a clean UI
