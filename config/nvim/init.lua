af2c01f348450c010d1d47a64d3c7e9fa9223cc9
Álysson Gleyson da Silva <alyssongleyson.dev@gmail.com>
2026-08-22 05:19:20 -0300

    feat(nvim): configure obsidian.nvim plugin for note management

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -42,4 +42,27 @@ require("lazy").setup({
     tag = "0.1.8",
     dependencies = { "nvim-lua/plenary.nvim" },
   },
+
+  -- Obsidian integration for note-taking
+  {
+    "epwalsh/obsidian.nvim",
+    version = "*",
+    lazy = true,
+    ft = "markdown",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    opts = {
+      workspaces = {
+        {
+          name = "nvim.notes",
+          path = "~/mdnotes",
+        },
+      },
+      daily_notes = {
+        folder = "log",
+        date_format = "%Y-%m-%d",
+      },
+    },
+  },
 })
