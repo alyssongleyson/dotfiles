a5382646eead3d0c53b2787e050fd067ca2f4e5a
Álysson Gleyson da Silva <alyssongleyson.dev@gmail.com>
2026-08-25 22:54:23 -0300

    feat(nvim): add filetype configuration for rst files

---
+++ config/nvim/after/ftplugin/rst.lua
@@ -0,0 +1,8 @@
+-- Filetype configuration for reStructuredText (.rst)
+vim.opt_local.tabstop = 2
+vim.opt_local.shiftwidth = 2
+vim.opt_local.softtabstop = 2
+vim.opt_local.expandtab = true
+vim.opt_local.textwidth = 80
+vim.opt_local.wrap = false
+
